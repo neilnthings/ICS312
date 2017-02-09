53e96f73ddc7a44c908b628d3723f3dd2d4a8f66
neilnthings <tevesnc@gmail.com>
2017-02-08 15:10:41 -1000

    changed formatting

--- hw3/ex1/hw3_ex1.asm
+++ hw3/ex1/hw3_ex1.asm
@@ -1,32 +1,32 @@
 %include "asm_io.inc"
 
 segment .data
-	message		db		"First '0' was entered then '0' was entered",0
+    message     db      "First '0' was entered then '0' was entered",0
 
 segment .bss
 
 segment .text
-	global asm_main
+    global asm_main
 asm_main:
-	enter	0,0
-	pusha
+    enter   0,0
+    pusha
 
-	call read_char
-	mov bl, al
-	call read_char
-	call read_char
-	mov cl, al
-	call read_char
-	mov eax, message
-	add eax, 7
-	mov [eax], bl
-	add eax, 21
-	mov [eax], cl
-	sub eax, 28
-	call print_string
-	call print_nl
+    call        read_char
+    mov         bl, al
+    call        read_char
+    call        read_char
+    mov         cl, al
+    call        read_char
+    mov         eax, message
+    add         eax, 7
+    mov         [eax], bl
+    add         eax, 21
+    mov         [eax], cl
+    sub         eax, 28
+    call        print_string
+    call        print_nl
 
-	popa
-	mov	eax, 0
-	leave
-	ret
+    popa
+    mov eax, 0
+    leave
+    ret
